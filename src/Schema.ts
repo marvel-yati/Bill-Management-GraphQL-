98f8e8929982917c3ff117526528ac903c4c8b38
marvel-yati <debayatisarkar@gmail.com>
2023-01-19 12:12:42 +0530

    firstPush

---
+++ src/Schema.ts
@@ -0,0 +1,23 @@
+import { gql } from "apollo-server-express"; //will create a schema
+const Schema = gql`
+  type Bill {
+    id: ID!
+    description: String
+    category: String
+    amount: String
+    date: String
+  }
+  #handle bills commands
+  type Query {
+    getAllBills: [Bill] #will return multiple Person instances
+    getBill(id: Int): Bill 
+  }
+  type Mutation{
+    addBill(description:String,category:String,amount:String,date:String): Bill
+    addBillAtAnyPosition(id:Int!,description:String!,category:String!,amount:String,date:String): Bill
+    updateBill(id:Int!,description:String!,category:String!,amount:String,date:String): Bill
+    deleteBill(id:Int!):Bill
+  }
+`;
+export default Schema; 
+//export this Schema so we can use it in our project
---
+++ src/dataset.ts
@@ -0,0 +1,33 @@
+let bills: { id:number;description:string;category:string;amount:string;date:string}[] = [
+    {
+        id: 1,
+        description: "Dominoes",
+        category: "FoodNDining",
+        amount: "43000",
+        date: "01-02-2020"
+    },
+    {
+        id: 2,
+        description: "Car wash",
+        category: "utility",
+        amount: "8000",
+        date: "01-06-2020"
+    },
+    {
+        id: 3,
+        description: "Car wash",
+        category: "utility",
+        amount: "7000",
+        date: "01-06-2020"
+    },
+    {
+      id: 4,
+      description: "Air Conditioning",
+      category: "Cool",
+      amount: "4000",
+      date: "17-09-2021"
+    }
+]
+
+export default bills
+
